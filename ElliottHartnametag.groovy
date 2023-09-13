8ba3fbe0c76cf2d019d63071a227424fefca9b1b
Elliott1807 <144172739+Elliott1807@users.noreply.github.com>
2023-09-13 11:15:13 -0400

    changes to text

--- ElliottHartnametag.groovy
+++ ElliottHartnametag.groovy
@@ -1,5 +1,19 @@
 // code here
 
+import eu.mihosoft.vrl.v3d.*;
+import javafx.scene.text.Font;
+
+Font font = new Font("Arial",  30);
+
+CSG text = CSG.unionAll(
+	TextExtrude.text((double)2,"Elliott",font).collect{
+		it.rotx(180)
+		.toZMin()
+	}
+)
+	
+
+
 double width = 30
 double depth = 70
 double height = 3
@@ -13,4 +27,4 @@ println "height is "+height
 
 CSG nameTagBase = new Cube(width,depth,height).toCSG()
 
-return nameTagBase
+return [nameTagBase,text]
